680554c769f18fcf6f6573193f15eb595d0e4d5f
Jake Luong <luong_quoc_hao@sezax.co.jp>
2020-07-15 19:22:14 +0700

    quiz-app done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-import './question.dart';
+import './quiz.dart';
+import './result.dart';
 
 // void main() {
 //   runApp(QuizApp());
@@ -17,44 +18,68 @@ class QuizApp extends StatefulWidget {
 }
 
 class _QuizAppState extends State<QuizApp> {
+  final _questions = const [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'White', 'score': 1},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3}
+      ]
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': [
+        {'text': 'Shark', 'score': 10},
+        {'text': 'Dolphin', 'score': 3},
+        {'text': 'Elephant', 'score': 2},
+        {'text': 'Tiger', 'score': 5}
+      ]
+    },
+    {
+      'questionText': 'Who\'s your favorite hero?',
+      'answers': [
+        {'text': 'Arrow', 'score': 10},
+        {'text': 'The Flash', 'score': 5},
+        {'text': 'Superman', 'score': 2},
+        {'text': 'Batman', 'score': 8}
+      ]
+    },
+  ];
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
+
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
+    if (_questionIndex < _questions.length) {}
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      'What \'s your favorite color?',
-      'What \'s your favorite animal'
-    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Quiz App'),
         ),
-        body: Column(
-          children: [
-            Question(
-              questions[_questionIndex],
-            ),
-            RaisedButton(
-              child: Text('Answer 1'),
-              onPressed: _answerQuestion,
-            ),
-            RaisedButton(
-              child: Text('Answer 2'),
-              onPressed: null,
-            ),
-            RaisedButton(
-              child: Text('Answer 3'),
-              onPressed: null,
-            ),
-          ],
-        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(_totalScore, _resetQuiz),
       ),
     );
   }
