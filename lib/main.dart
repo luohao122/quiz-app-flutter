7cce4290ded6166b34336271b1ba9b02903c58d6
Jake Luong <luong_quoc_hao@sezax.co.jp>
2020-07-15 16:27:29 +0700

    flutter-quiz

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+import './question.dart';
+
+// void main() {
+//   runApp(QuizApp());
+// }
+
+void main() => runApp(QuizApp());
+
+class QuizApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _QuizAppState();
+  }
+}
+
+class _QuizAppState extends State<QuizApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      'What \'s your favorite color?',
+      'What \'s your favorite animal'
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Quiz App'),
+        ),
+        body: Column(
+          children: [
+            Question(
+              questions[_questionIndex],
+            ),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: _answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: null,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
